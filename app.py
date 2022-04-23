649c58fd525d8934e8d9187e55e749f1d6c1a1ed
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-04-23 14:48:04 +0530

    Initial commit
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, jsonify
+from flask_restful import Api
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about.html')
+def about_html():
+    return render_template('about.html')
+
+
+@app.route('/cart.html')
+def cart_html():
+    return render_template('cart.html')
+
+
+@app.route('/checkout.html')
+def checkout_html():
+    return render_template('checkout.html')
+
+
+@app.route('/contact.html')
+def contact_html():
+    return render_template('contact.html')
+
+
+@app.route('/index.html')
+def index_html():
+    return render_template('index.html')
+
+
+@app.route('/login.html')
+def login_html():
+    return render_template('login.html')
+
+
+@app.route('/my-account.html')
+def my_account_html():
+    return render_template('my-account.html')
+
+
+@app.route('/order.html')
+def order_html():
+    return render_template('order.html')
+
+
+@app.route('/shop.html')
+def shop_html():
+    return render_template('shop.html')
+
+
+@app.route('/single-product.html')
+def single_product():
+    return render_template('single-product.html')
+
+
+@app.route('/wishlist.html')
+def wishlist_html():
+    return render_template('wishlist.html')
